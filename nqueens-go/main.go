caf3df93325e7493e28157e66b5e4f871e4b17ff
Nenad Stojanovikj <nst@hellofresh.com>
2020-05-28 17:54:30 +0200

    Clean up main

--- nqueens-go/main.go
+++ nqueens-go/main.go
@@ -17,7 +17,7 @@ type Configuration struct {
 
 func loadConfig() Configuration {
 	var (
-		population     = flag.Int("population", 5000, "Sets the population")
+		population     = flag.Int("population", 1000, "Sets the population")
 		mutationChance = flag.Float64("mutation", 0.02, "Sets the mutation chance")
 		boardSize      = flag.Int("boardSize", 8, "Sets the NxN board size with N queens")
 		maxGenerations = flag.Int("maxGenerations", 5000, "Sets the maximum number of generations")
@@ -43,14 +43,13 @@ func main() {
 
 func run(cfg Configuration) error {
 	population := generatePopulation(cfg.Population, cfg.BoardSize)
+	n := len(population)
 	calculateFitness(population)
 
 	maxDNA := DNA{}
-
 	for gen := 0; gen <= cfg.MaxGenerations; gen++ {
 		// Compare fitness
 		totalFitness := float64(0)
-		n := len(population)
 
 		for i := 0; i < n; i++ {
 			if population[i].Fitness == 1 {
@@ -63,45 +62,49 @@ func run(cfg Configuration) error {
 			totalFitness += population[i].Fitness
 		}
 
-		newGen := make([]DNA, cfg.Population)
-		for i := 0; i < cfg.Population; i++ {
-			r1 := rand.Float64() * totalFitness
-			r2 := rand.Float64() * totalFitness
-
-			var p1, p2 *DNA
-
-			gotParents := 0
-			// best parent selection
-			for j := 0; j < n; j++ {
-				r1 -= population[j].Fitness
-				if r1 <= 0 && p1 == nil {
-					p1 = &population[j]
-					gotParents++
-				}
-
-				r2 -= population[j].Fitness
-				if r2 <= 0  && p2 == nil{
-					p2 = &population[j]
-					gotParents++
-				}
-
-				if gotParents == 2 {
-					break
-				}
-			}
-
-			child := p1.Crossover(*p2)
-			child.Mutate(cfg.MutationChance)
-			child.CalculateFitness()
-			newGen[i] = child
-		}
-
-		population = newGen
-
+		population = selectNextGeneration(population, totalFitness, cfg.MutationChance)
 		fmt.Printf("Gen: %d, Max fitness now: %.2f\r", gen, maxDNA.Fitness)
 	}
 
 	fmt.Printf("Best candidate: %#v\n", maxDNA)
-
 	return nil
+}
+
+func selectNextGeneration(population []DNA, totalFitness float64, mutationChance float64) []DNA {
+	n := len(population)
+	nextGen := make([]DNA, n)
+
+	for i := 0; i < n; i++ {
+		r1 := rand.Float64() * totalFitness
+		r2 := rand.Float64() * totalFitness
+
+		var p1, p2 *DNA
+
+		gotParents := 0
+		// best parent selection
+		for j := 0; j < n; j++ {
+			r1 -= population[j].Fitness
+			if r1 <= 0 && p1 == nil {
+				p1 = &population[j]
+				gotParents++
+			}
+
+			r2 -= population[j].Fitness
+			if r2 <= 0 && p2 == nil {
+				p2 = &population[j]
+				gotParents++
+			}
+
+			if gotParents == 2 {
+				break
+			}
+		}
+
+		child := p1.Crossover(*p2)
+		child.Mutate(mutationChance)
+		child.CalculateFitness()
+		nextGen[i] = child
+	}
+
+	return nextGen
 }
